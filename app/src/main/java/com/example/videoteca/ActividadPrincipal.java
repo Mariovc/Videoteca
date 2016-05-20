3e177bd4dcb16dfbca87968eca7883032caa369f
Mario Velasco Casquero <m3ario@gmail.com>
2016-05-20 14:51:14 +0200

    First commit

---
+++ app/src/main/java/com/example/videoteca/ActividadPrincipal.java
@@ -0,0 +1,16 @@
+package com.example.videoteca;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Author: Mario Velasco Casquero
+ * Date: 20/05/2016
+ */
+public class ActividadPrincipal extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
